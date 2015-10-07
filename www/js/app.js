8a8facb1f8756d909a820804b971d63e1859916f
mahesh maurya <mahesh@wohlig.com>
2015-10-07 17:58:06 +0530

    git login liknk

--- www/js/app.js
+++ www/js/app.js
@@ -211,7 +211,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 			}
 		});
 	// if none of the above states are matched, use this as the fallback
-	$urlRouterProvider.otherwise('/app/home');
+	$urlRouterProvider.otherwise('/login');
 })
 
 .directive('onlyDigits', function () {
