b697fe4a1d24e0075cbe4467c3eb51651a813535
jagruti181 <patiljagruti181@gmail.com>
2015-10-16 17:28:57 +0530

    my git

--- www/js/app.js
+++ www/js/app.js
@@ -296,7 +296,7 @@ angular.module('starter', ['ionic', 'ionic.service.core', 'starter.controllers',
 			}
 		});
 	// if none of the above states are matched, use this as the fallback
-	$urlRouterProvider.otherwise('/home');
+	$urlRouterProvider.otherwise('/app/home');
 })
 
 .directive('onlyDigits', function () {
